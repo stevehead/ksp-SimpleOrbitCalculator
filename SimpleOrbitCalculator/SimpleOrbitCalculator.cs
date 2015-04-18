ca7ac9edb9a85f9dfa44edfcaad985e8d56be7c1
Steve Johnson <sjohnson0684@gmail.com>
2015-04-18 13:30:26 -0400

    Stock toolbar icon added.

--- SimpleOrbitCalculator/SimpleOrbitCalculator.cs
+++ SimpleOrbitCalculator/SimpleOrbitCalculator.cs
@@ -6,17 +6,35 @@ using UnityEngine;
 
 namespace SimpleOrbitCalculator
 {
-    [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
     public class OrbitCalculatorGUIController : UnityEngine.MonoBehaviour
     {
+        private const string PluginName = "Simple Orbit Calculator";
+        private const string PluginDirectoryName = "SimpleOrbitCalculator";
+        private const string PluginIconButtonStock = "icon_button_stock";
+
+        private bool windowOpen = false;
+        private Rect windowPos = new Rect(100, 100, 800, 465);
+
+        private ApplicationLauncherButton SOCButtonStock = null;
+
         public void Awake()
         {
-
+            GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
         }
 
         void OnGUIAppLauncherReady()
         {
-
+            if (ApplicationLauncher.Ready)
+            {
+                string pluginIconButtonStockPath = String.Format("{0}/Textures/{1}", PluginDirectoryName, PluginIconButtonStock);
+                SOCButtonStock = ApplicationLauncher.Instance.AddModApplication(
+                    OnAppLaunchToggleOn, OnAppLaunchToggleOff,
+                    DummyVoid, DummyVoid,
+                    DummyVoid, DummyVoid,
+                    ApplicationLauncher.AppScenes.SPACECENTER,
+                    (Texture)GameDatabase.Instance.GetTexture(pluginIconButtonStockPath, false));
+            }
         }
 
         void OnAppLaunchToggleOn()
@@ -33,12 +51,17 @@ namespace SimpleOrbitCalculator
 
         public void Start()
         {
-
+            RenderingManager.AddToPostDrawQueue(0, new Callback(DrawGUI));
+            print(PluginName + "GUI Loaded");
         }
 
         public void DrawGUI()
         {
-
+            if (windowOpen)
+            {
+                windowPos = GUILayout.Window(PluginName.GetHashCode(), windowPos, MainWindow, PluginName,
+                    GUILayout.Width(800), GUILayout.Height(465), GUILayout.ExpandWidth(false));
+            }
         }
 
         private void MainWindow(int windowID)
@@ -48,7 +71,11 @@ namespace SimpleOrbitCalculator
 
         private void OnDestroy()
         {
-
+            GameEvents.onGUIApplicationLauncherReady.Remove(OnGUIAppLauncherReady);
+            if (SOCButtonStock != null)
+            {
+                ApplicationLauncher.Instance.RemoveModApplication(SOCButtonStock);
+            }
         }
     }
 }
