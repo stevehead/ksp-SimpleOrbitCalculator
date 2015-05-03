a4849681265a16527c0b7ca1f20541cc62439d39
Steve Johnson <sjohnson0684@gmail.com>
2015-05-03 05:36:18 -0400

    First cleanup.

--- SimpleOrbitCalculator/SimpleOrbitCalculator.cs
+++ SimpleOrbitCalculator/SimpleOrbitCalculator.cs
@@ -7,61 +7,115 @@ using UnityEngine;
 namespace SimpleOrbitCalculator
 {
     [KSPAddon(KSPAddon.Startup.EveryScene, false)]
-    public class OrbitCalculatorGUIController : UnityEngine.MonoBehaviour
+    public class OrbitCalculatorGUIController : MonoBehaviour
     {
+        /// <summary>
+        /// The plugin's name.
+        /// </summary>
         private const string PluginName = "Simple Orbit Calculator";
+
+        /// <summary>
+        /// The plugin's directory under GameData.
+        /// </summary>
         private const string PluginDirectoryName = "SimpleOrbitCalculator";
+
+        /// <summary>
+        /// The stock toolbar's icon.
+        /// </summary>
         private const string PluginIconButtonStock = "icon_button_stock";
 
+        /// <summary>
+        /// Is calculator open?
+        /// </summary>
         private static bool windowOpen = false;
-        private bool isActivated = false;
+
+        /// <summary>
+        /// Location of the calculator's window.
+        /// </summary>
         private Rect windowPos = new Rect(100, 100, 800, 465);
 
+        /// <summary>
+        /// The application launcher button that is created.
+        /// </summary>
         private static ApplicationLauncherButton appLauncherButton = null;
 
-        void OnAppLaunchToggleOn()
+        /// <summary>
+        /// The scenes the button will be visible in.
+        /// </summary>
+        private static ApplicationLauncher.AppScenes ScenesApplicationVisibleIn
+        {
+            get
+            {
+                return ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.MAPVIEW | ApplicationLauncher.AppScenes.SPACECENTER
+                        | ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.TRACKSTATION | ApplicationLauncher.AppScenes.VAB;
+            }
+        }
+
+        /// <summary>
+        /// Called when the script is loaded.
+        /// </summary>
+        public void Awake()
+        {
+            // If the application has not been registered yet.
+            if (appLauncherButton == null)
+            {
+                GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
+                GameEvents.onGUIApplicationLauncherDestroyed.Add(OnGuiAppLauncherDestroyed);
+                GameEvents.onGameSceneLoadRequested.Add(OnGameSceneLoadRequestedForAppLauncher);
+            }
+
+            // If this is a valid scene, add the GUI callback.
+            if (IsValidScene())
+            {
+                RenderingManager.AddToPostDrawQueue(3, new Callback(DrawGUI));
+            }
+        }
+
+        /// <summary>
+        /// Sets the window as open. Should only be called by a button.
+        /// </summary>
+        private void OnAppLaunchToggleOn()
         {
             if (appLauncherButton == null)
             {
                 Debug.LogError(PluginName + " :: OnAppLaunchToggleOn called without a button.");
-                return;
             }
 
             windowOpen = true;
         }
 
-        void OnAppLaunchToggleOff()
+        /// <summary>
+        /// Sets the windows as closed. Should only be called by a button.
+        /// </summary>
+        private void OnAppLaunchToggleOff()
         {
             if (appLauncherButton == null)
             {
                 Debug.LogError(PluginName + " :: OnAppLaunchToggleOff called without a button.");
-                return;
             }
+
             windowOpen = false;
         }
 
-        void RemoveFromAppLauncher()
+        /// <summary>
+        /// Removes the application from launcher.
+        /// </summary>
+        private void RemoveFromAppLauncher()
         {
-#if DEBUG
-            Debug.Log(PluginName + " :: RemoveFromAppLauncher called.");
-#endif
             if (appLauncherButton != null)
             {
                 ApplicationLauncher.Instance.RemoveApplication(appLauncherButton);
-                GameEvents.onGUIApplicationLauncherReady.Remove(OnGUIAppLauncherReady);
-                GameEvents.onGUIApplicationLauncherReady.Remove(OnGuiAppLauncherDestroyed);
-                GameEvents.onGameSceneLoadRequested.Remove(OnGameSceneLoadRequestedForAppLauncher);
-#if DEBUG
-                Debug.Log(PluginName + " :: RemoveFromAppLauncher removal logic completed.");
-#endif
             }
+            GameEvents.onGUIApplicationLauncherReady.Remove(OnGUIAppLauncherReady);
+            GameEvents.onGUIApplicationLauncherReady.Remove(OnGuiAppLauncherDestroyed);
+            GameEvents.onGameSceneLoadRequested.Remove(OnGameSceneLoadRequestedForAppLauncher);
         }
 
-        void OnGUIAppLauncherReady()
+        /// <summary>
+        /// Called when the AppLauncher is ready to receive this application.
+        /// </summary>
+        private void OnGUIAppLauncherReady()
         {
-#if DEBUG
-            Debug.Log(PluginName + " :: OnGuiAppLauncherReady called.");
-#endif
             if (ApplicationLauncher.Ready && appLauncherButton == null)
             {
                 string pluginIconButtonStockPath = String.Format("{0}/Textures/{1}", PluginDirectoryName, PluginIconButtonStock);
@@ -69,35 +123,49 @@ namespace SimpleOrbitCalculator
                     OnAppLaunchToggleOn, OnAppLaunchToggleOff,
                     null, null,
                     null, null,
-                    ApplicationLauncher.AppScenes.FLIGHT | ApplicationLauncher.AppScenes.MAPVIEW | ApplicationLauncher.AppScenes.SPACECENTER
-                        | ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.TRACKSTATION | ApplicationLauncher.AppScenes.VAB,
+                    ScenesApplicationVisibleIn,
                     (Texture)GameDatabase.Instance.GetTexture(pluginIconButtonStockPath, false));
-#if DEBUG
-                Debug.Log(PluginName + " :: AppLauncher menu item added.");
-#endif
             }
         }
 
-        void OnGuiAppLauncherDestroyed()
+        /// <summary>
+        /// Called when the App Launcher toolbar is destroyed.
+        /// </summary>
+        private void OnGuiAppLauncherDestroyed()
         {
-#if DEBUG
-            Debug.Log(PluginName + " :: OnGuiAppLauncherDestroyed called.");
-#endif
-            if (appLauncherButton != null)
+            RemoveFromAppLauncher();
+        }
+
+        /// <summary>
+        /// Called when the game scene changes.
+        /// </summary>
+        /// <param name="sceneToLoad">The new game scene.</param>
+        private void OnGameSceneLoadRequestedForAppLauncher(GameScenes sceneToLoad)
+        {
+            RemoveFromAppLauncher();
+        }
+
+        /// <summary>
+        /// Handles the GUI rendering.
+        /// </summary>
+        private void DrawGUI()
+        {
+            if (IsValidScene() && windowOpen)
             {
-                RemoveFromAppLauncher();
+                if (windowOpen)
+                {
+                    windowPos = GUILayout.Window(PluginName.GetHashCode(), windowPos, MainWindow, PluginName, GUILayout.Width(800));
+                }
             }
         }
 
-        void OnGameSceneLoadRequestedForAppLauncher(GameScenes sceneToLoad)
+        /// <summary>
+        /// The rendering of the calculator.
+        /// </summary>
+        /// <param name="windowID">Window ID</param>
+        private void MainWindow(int windowID)
         {
-#if DEBUG
-            Debug.Log(PluginName + " :: OnGameSceneLoadRequestedForAppLauncher called.");
-#endif
-            if (appLauncherButton != null)
-            {
-                RemoveFromAppLauncher();
-            }
+            CalculatorWindow.Instance.Render();
         }
 
         /// <summary>
@@ -105,47 +173,10 @@ namespace SimpleOrbitCalculator
         /// This plugin should be able to run in VAB/SPH, Flight, Space Center, and Tracking Station scenes.
         /// </summary>
         /// <returns>True if valid; false if not valid.</returns>
-        Boolean IsValidScene()
+        private static bool IsValidScene()
         {
-            return HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight || HighLogic.LoadedScene == GameScenes.SPACECENTER || HighLogic.LoadedScene == GameScenes.TRACKSTATION;
-        }
-
-        public void Awake()
-        {
-#if DEBUG
-            Debug.Log(PluginName + " :: is now awake.");
-#endif
-            if (appLauncherButton == null)
-            {
-#if DEBUG
-                Debug.Log(PluginName + " :: AppLauncher setup.");
-#endif
-                GameEvents.onGUIApplicationLauncherReady.Add(OnGUIAppLauncherReady);
-                GameEvents.onGUIApplicationLauncherDestroyed.Add(OnGuiAppLauncherDestroyed);
-                GameEvents.onGameSceneLoadRequested.Remove(OnGameSceneLoadRequestedForAppLauncher);
-            }
-
-            if (IsValidScene())
-            {
-                RenderingManager.AddToPostDrawQueue(3, new Callback(DrawGUI));
-            }
-        }
-
-        public void DrawGUI()
-        {
-            if (IsValidScene())
-            {
-                if (windowOpen)
-                {
-                    windowPos = GUILayout.Window(PluginName.GetHashCode(), windowPos, MainWindow, PluginName, GUILayout.Width(800));
-                }
-                isActivated = windowOpen;
-            }
-        }
-
-        private void MainWindow(int windowID)
-        {
-            CalculatorWindow.Instance.Render();
+            return HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight || HighLogic.LoadedScene == GameScenes.SPACECENTER
+                || HighLogic.LoadedScene == GameScenes.TRACKSTATION;
         }
     }
 }
